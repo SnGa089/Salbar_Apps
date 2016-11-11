8649f814c6a2c329c3eef1714228a8bfd0dc8f80
Sebastian Gamarra <SnGa0089@gmail.com>
2016-11-11 09:23:31 -0300

    Probando la sincronizacion con el repositorio

--- Proyecto Salbar/Form1.vb
+++ Proyecto Salbar/Form1.vb
@@ -1,3 +1,5 @@
 ﻿Public Class Form1
-
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        MsgBox("Probando la sincronizacion con el Repositorio")
+    End Sub
 End Class
